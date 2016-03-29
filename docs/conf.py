b064e7dfdc1d17b8fa6b8eab390305b9146d5972
Taras Voinarovskiy <taras.voinarovskiy@zabs.co>
2016-03-29 13:31:47 +0300

    Added sphinx theme and sphinxcontrib-asyncio

--- docs/conf.py
+++ docs/conf.py
@@ -57,6 +57,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.viewcode',
+    'sphinxcontrib.asyncio'
 ]
 
 intersphinx_mapping = {'python': ('http://docs.python.org/3', None)}
@@ -130,13 +131,17 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if on_rtd:
     html_theme = 'default'
 else:
-    html_theme = 'pyramid'
+    import sphinx_rtd_theme
+
+    html_theme = "sphinx_rtd_theme"
+
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # -- Options for HTML output ----------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+# html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
