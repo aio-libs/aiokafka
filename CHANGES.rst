a00e43a8f005bdd8a032ce6ebd5176e93aa0d26b
Taras <voyn1991@gmail.com>
2016-04-15 13:03:36 +0300

    Bump version v0.0.1 -> v0.1.0. Initial release

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,9 @@
 CHANGES
 --------
 
-0.0.1 (XXXX-XX-XX)
+0.1.0 (2016-04-15)
 ^^^^^^^^^^^^^^^^^^
 
 Initial release
+
+Added full support for Kafka 9.0. Older Kafka versions are not tested.
--- aiokafka/__init__.py
+++ aiokafka/__init__.py
@@ -6,7 +6,7 @@ except ImportError:
 import sys  # noqa
 PY_35 = sys.version_info >= (3, 5)
 
-__version__ = '0.0.1'
+__version__ = '0.1.0'
 
 from .client import AIOKafkaClient  # noqa
 from .producer import AIOKafkaProducer  # noqa
