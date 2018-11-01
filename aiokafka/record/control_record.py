8699c3620b4c13ec3dd842dc5c83b924ff384b8f
Taras <voyn1991@gmail.com>
2018-11-01 21:25:47 +0200

    Add control_record file

---
+++ aiokafka/record/control_record.py
@@ -0,0 +1,31 @@
+import struct
+
+
+class ControlRecord:
+
+    def __init__(self, version, type_):
+        self._version = version
+        self._type = type_
+
+    @property
+    def version(self):
+        return self._version
+
+    @property
+    def type_(self):
+        return self._type
+
+    def __eq__(self, other):
+        if isinstance(other, ControlRecord):
+            return other._version == self._version and \
+                other._type == self._type
+        return False
+
+    @classmethod
+    def parse(cls, data: bytes, _schema=struct.Struct(">HH")):
+        version, type_ = _schema.unpack_from(data)
+        return cls(version, type_)
+
+
+ABORT_MARKER = ControlRecord(0, 0)
+COMMIT_MARKER = ControlRecord(0, 1)
