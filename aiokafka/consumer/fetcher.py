fbde297e4846058d70b9a1fccc4571897c778cce
RAJAT SINGH <iamsinghrajat@gmail.com>
2020-06-10 00:30:30 +0530

    hotfix/ memory leak in comsumer getmany

--- aiokafka/consumer/fetcher.py
+++ aiokafka/consumer/fetcher.py
@@ -1101,6 +1101,9 @@ class Fetcher:
                 [waiter], timeout=timeout, loop=self._loop)
 
             if not done or self._closed:
+                fut = _.pop()
+                self._fetch_waiters.discard(fut)
+                fut.cancel()
                 return {}
 
             if waiter.done():
