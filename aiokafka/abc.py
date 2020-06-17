3791cae77a7c7ca4b096b05d4d11e2b84e99a258
Andy Luo <andy.c.luo@gmail.com>
2020-06-17 18:21:05 +1000

    offered our indenpendent AbastractTokenProvider

--- aiokafka/abc.py
+++ aiokafka/abc.py
@@ -1,8 +1,5 @@
 import abc
 from kafka import ConsumerRebalanceListener as BaseConsumerRebalanceListener
-from kafka.oauth.abstract import AbstractTokenProvider \
-    as BaseAbstractTokenProvider
-import asyncio
 
 
 class ConsumerRebalanceListener(BaseConsumerRebalanceListener):
@@ -90,14 +87,54 @@ class ConsumerRebalanceListener(BaseConsumerRebalanceListener):
         pass
 
 
-class AbstractTokenProvider(BaseAbstractTokenProvider):
-    async def token(self):
-        return await asyncio.get_running_loop().run_in_executor(
-            None, self._token)
+# This statement is compatible with both Python 2.7 & 3+
+ABC = abc.ABCMeta('ABC', (object,), {'__slots__': ()})
+
+
+class AbstractTokenProvider(ABC):
+    """
+    A Token Provider must be used for the SASL OAuthBearer protocol.
+    The implementation should ensure token reuse so that multiple
+    calls at connect time do not create multiple tokens. The implementation
+    should also periodically refresh the token in order to guarantee
+    that each call returns an unexpired token. A timeout error should
+    be returned after a short period of inactivity so that the
+    broker can log debugging info and retry.
+    Token Providers MUST implement the token() method
+    """
+
+    def __init__(self, **config):
+        pass
 
     @abc.abstractmethod
-    def _token(self):
+    async def token(self):
+        """
+        An async callback returning a (str) ID/Access Token to be sent to
+        the Kafka client. In case where a synchoronous callback is needed,
+        implementations like following can be used:
+        .. highlight:: python
+        .. code-block:: python
+            from aiokafka.abc import AbstractTokenProvider
+
+            class CustomTokenProvider(AbstractTokenProvider):
+                async def token(self):
+                    return asyncio.get_running_loop().run_in_executor(
+                        None, self._token)
+
+                def _token(self):
+                    # The actual synchoronous token callback.
+        """
         pass
+
+    def extensions(self):
+        """
+        This is an OPTIONAL method that may be implemented.
+        Returns a map of key-value pairs that can
+        be sent with the SASL/OAUTHBEARER initial client request. If
+        not implemented, the values are ignored. This feature is only available
+        in Kafka >= 2.1.0.
+        """
+        return {}
 
 
 __all__ = [
