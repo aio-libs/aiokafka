d02ddd9d3f02d41ef64dcf3bf985a3015be004ec
Vikram Patki 24489 <vpatki@wayfair.com>
2020-11-25 11:16:30 -0500

    fixing linting errors

--- aiokafka/protocol/group.py
+++ aiokafka/protocol/group.py
@@ -5,22 +5,26 @@ from kafka.protocol.struct import Struct
 from kafka.protocol.types import Array, Bytes, Int16, Int32, Schema, String
 from kafka.protocol.group import *
 
+
 class JoinGroupResponse_v5(Response):
     API_KEY = 11
     API_VERSION = 5
     SCHEMA = Schema(
-        ('throttle_time_ms', Int32),
-        ('error_code', Int16),
-        ('generation_id', Int32),
-        ('group_protocol', String('utf-8')),
-        ('leader_id', String('utf-8')),
-        ('member_id', String('utf-8')),
-        ('members', Array(
-            ('member_id', String('utf-8')),
-            ('group_instance_id', String('utf-8')),
-            ('member_metadata', Bytes)))
+        ("throttle_time_ms", Int32),
+        ("error_code", Int16),
+        ("generation_id", Int32),
+        ("group_protocol", String("utf-8")),
+        ("leader_id", String("utf-8")),
+        ("member_id", String("utf-8")),
+        (
+            "members",
+            Array(
+                ("member_id", String("utf-8")),
+                ("group_instance_id", String("utf-8")),
+                ("member_metadata", Bytes),
+            ),
+        ),
     )
-
 
 
 class JoinGroupRequest_v5(Request):
@@ -28,24 +32,31 @@ class JoinGroupRequest_v5(Request):
     API_VERSION = 5
     RESPONSE_TYPE = JoinGroupResponse_v5
     SCHEMA = Schema(
-        ('group', String('utf-8')),
-        ('session_timeout', Int32),
-        ('rebalance_timeout', Int32),
-        ('member_id', String('utf-8')),
-        ('group_instance_id', String('utf-8')),
-        ('protocol_type', String('utf-8')),
-        ('group_protocols', Array(
-            ('protocol_name', String('utf-8')),
-            ('protocol_metadata', Bytes)))
+        ("group", String("utf-8")),
+        ("session_timeout", Int32),
+        ("rebalance_timeout", Int32),
+        ("member_id", String("utf-8")),
+        ("group_instance_id", String("utf-8")),
+        ("protocol_type", String("utf-8")),
+        (
+            "group_protocols",
+            Array(("protocol_name", String("utf-8")), ("protocol_metadata", Bytes)),
+        ),
     )
-    UNKNOWN_MEMBER_ID = ''
+    UNKNOWN_MEMBER_ID = ""
 
 
 JoinGroupRequest = [
-    JoinGroupRequest_v0, JoinGroupRequest_v1, JoinGroupRequest_v2, JoinGroupRequest_v5
+    JoinGroupRequest_v0,
+    JoinGroupRequest_v1,
+    JoinGroupRequest_v2,
+    JoinGroupRequest_v5,
 ]
 JoinGroupResponse = [
-    JoinGroupResponse_v0, JoinGroupResponse_v1, JoinGroupRequest_v2, JoinGroupResponse_v5
+    JoinGroupResponse_v0,
+    JoinGroupResponse_v1,
+    JoinGroupRequest_v2,
+    JoinGroupResponse_v5,
 ]
 
 
@@ -60,15 +71,15 @@ class SyncGroupRequest_v3(Request):
     API_VERSION = 3
     RESPONSE_TYPE = SyncGroupResponse_v3
     SCHEMA = Schema(
-        ('group', String('utf-8')),
-        ('generation_id', Int32),
-        ('member_id', String('utf-8')),
-        ('group_instance_id', String('utf-8')),
-        ('group_assignment', Array(
-            ('member_id', String('utf-8')),
-            ('member_metadata', Bytes)))
+        ("group", String("utf-8")),
+        ("generation_id", Int32),
+        ("member_id", String("utf-8")),
+        ("group_instance_id", String("utf-8")),
+        (
+            "group_assignment",
+            Array(("member_id", String("utf-8")), ("member_metadata", Bytes)),
+        ),
     )
-
 
 
 SyncGroupRequest = [SyncGroupRequest_v0, SyncGroupRequest_v1, SyncGroupRequest_v3]
