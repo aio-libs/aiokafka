77747f22cce499608b857a1ea718baaed347e2f5
g-clef <spamblock@g-clef.net>
2020-12-11 09:09:49 -0500

    fix trailing space

--- aiokafka/errors.py
+++ aiokafka/errors.py
@@ -419,7 +419,7 @@ class MemberIdRequired(BrokerResponseError):
     errno = 79
     message = 'MEMBER_ID_REQUIRED'
     description = (
-        'Consumer needs to have a valid member ' 
+        'Consumer needs to have a valid member '
         'id before actually entering group'
     )
 
