9eb6379f3880f3ff43688a083d8d621a17f484be
Paweł Rubin <32072144+pawelrubin@users.noreply.github.com>
2021-08-25 16:10:35 +0200

    Add SASL_PLAINTEXT and SASL_SSL to valid values in security protocol documentation. (#768)

--- aiokafka/producer/producer.py
+++ aiokafka/producer/producer.py
@@ -131,7 +131,8 @@ class AIOKafkaProducer:
             If set to 'auto', will attempt to infer the broker version by
             probing various APIs. Default: auto
         security_protocol (str): Protocol used to communicate with brokers.
-            Valid values are: PLAINTEXT, SSL. Default: PLAINTEXT.
+            Valid values are: PLAINTEXT, SSL, SASL_PLAINTEXT, SASL_SSL.
+            Default: PLAINTEXT.
         ssl_context (ssl.SSLContext): pre-configured SSLContext for wrapping
             socket connections. Directly passed into asyncio's
             `create_connection`_. For more information see :ref:`ssl_auth`.
