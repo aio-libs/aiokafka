680f74f5fe7c06aaabe3e499cd392ebe729b5a40
Denis Otkidach <denis.otkidach@gmail.com>
2023-11-15 18:53:11 +0200

    Switch to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,24 +1,10 @@
-import os
 import platform
-import re
 
+from Cython.Build import cythonize
 from setuptools import Extension, setup
 from setuptools.command.bdist_rpm import bdist_rpm as _bdist_rpm
 from setuptools.command.build_ext import build_ext
-
-
-try:
-    from setuptools.errors import CCompilerError, ExecError, PlatformError
-except ImportError:
-    # RTD workaround until it ships setuptools>=v59.0.0
-    # See:
-    # - https://github.com/pypa/setuptools/pull/2858
-    # - https://docs.readthedocs.io/en/stable/builds.html#python
-    from distutils.errors import (
-        CCompilerError,
-        DistutilsExecError as ExecError,
-        DistutilsPlatformError as PlatformError,
-    )
+from setuptools.errors import CCompilerError, ExecError, PlatformError
 
 
 # Those are needed to build _hton for windows
@@ -33,21 +19,11 @@ else:
     CFLAGS.extend(["-Wall", "-Wsign-compare", "-Wconversion"])
     LIBRARIES.append("z")
 
-# The extension part is copied from aiohttp's setup.py
-
-try:
-    from Cython.Build import cythonize
-
-    USE_CYTHON = True
-except ImportError:
-    USE_CYTHON = False
-
-ext = ".pyx" if USE_CYTHON else ".c"
 
 extensions = [
     Extension(
         "aiokafka.record._crecords.legacy_records",
-        ["aiokafka/record/_crecords/legacy_records" + ext],
+        ["aiokafka/record/_crecords/legacy_records.pyx"],
         libraries=LIBRARIES,
         extra_compile_args=CFLAGS,
         extra_link_args=LDFLAGS,
@@ -56,7 +32,7 @@ extensions = [
         "aiokafka.record._crecords.default_records",
         [
             "aiokafka/record/_crecords/crc32c.c",
-            "aiokafka/record/_crecords/default_records" + ext,
+            "aiokafka/record/_crecords/default_records.pyx",
         ],
         libraries=LIBRARIES,
         extra_compile_args=CFLAGS,
@@ -64,23 +40,19 @@ extensions = [
     ),
     Extension(
         "aiokafka.record._crecords.memory_records",
-        ["aiokafka/record/_crecords/memory_records" + ext],
+        ["aiokafka/record/_crecords/memory_records.pyx"],
         libraries=LIBRARIES,
         extra_compile_args=CFLAGS,
         extra_link_args=LDFLAGS,
     ),
     Extension(
         "aiokafka.record._crecords.cutil",
-        ["aiokafka/record/_crecords/crc32c.c", "aiokafka/record/_crecords/cutil" + ext],
+        ["aiokafka/record/_crecords/crc32c.c", "aiokafka/record/_crecords/cutil.pyx"],
         libraries=LIBRARIES,
         extra_compile_args=CFLAGS,
         extra_link_args=LDFLAGS,
     ),
 ]
-
-
-if USE_CYTHON:
-    extensions = cythonize(extensions)
 
 
 class bdist_rpm(_bdist_rpm):
@@ -110,84 +82,7 @@ class ve_build_ext(build_ext):
             raise BuildFailed()
 
 
-install_requires = [
-    "async-timeout",
-    "cramjam",
-    "packaging",
-]
-
-
-def read(f):
-    return open(os.path.join(os.path.dirname(__file__), f)).read().strip()
-
-
-extras_require = {
-    "snappy": ["cramjam"],
-    "lz4": ["lz4>=3.1.3"],
-    "zstd": ["cramjam"],
-    "gssapi": ["gssapi"],
-}
-extras_require["all"] = sum(extras_require.values(), [])
-
-
-def read_version():
-    regexp = re.compile(r"^__version__\W*=\W*'([\d.abrcdev]+)'")
-    init_py = os.path.join(os.path.dirname(__file__), "aiokafka", "__init__.py")
-    with open(init_py) as f:
-        for line in f:
-            match = regexp.match(line)
-            if match is not None:
-                return match.group(1)
-        else:
-            raise RuntimeError("Cannot find version in aiokafka/__init__.py")
-
-
-classifiers = [
-    "License :: OSI Approved :: Apache Software License",
-    "Intended Audience :: Developers",
-    "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.8",
-    "Programming Language :: Python :: 3.9",
-    "Programming Language :: Python :: 3.10",
-    "Programming Language :: Python :: 3.11",
-    "Operating System :: OS Independent",
-    "Topic :: System :: Networking",
-    "Topic :: System :: Distributed Computing",
-    "Framework :: AsyncIO",
-    "Development Status :: 4 - Beta",
-]
-
-
-args = dict(
-    name="aiokafka",
-    version=read_version(),
-    description=("Kafka integration with asyncio."),
-    long_description="\n\n".join((read("README.rst"), read("CHANGES.rst"))),
-    classifiers=classifiers,
-    platforms=["POSIX"],
-    author="Andrew Svetlov",
-    author_email="andrew.svetlov@gmail.com",
-    url="http://aiokafka.readthedocs.org",
-    project_urls={
-        "Source": "https://github.com/aio-libs/aiokafka",
-    },
-    download_url="https://pypi.python.org/pypi/aiokafka",
-    license="Apache 2",
-    packages=["aiokafka"],
-    python_requires=">=3.8",
-    install_requires=install_requires,
-    extras_require=extras_require,
-    include_package_data=True,
-    ext_modules=extensions,
+setup(
+    ext_modules=cythonize(extensions),
     cmdclass=dict(build_ext=ve_build_ext, bdist_rpm=bdist_rpm),
 )
-
-try:
-    setup(**args)
-except BuildFailed:
-    print("************************************************************")
-    print("Cannot compile C accelerator module, use pure python version")
-    print("************************************************************")
-    del args["ext_modules"]
-    del args["cmdclass"]
-    setup(**args)
